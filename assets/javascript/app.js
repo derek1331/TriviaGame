fe55f11f78c57fbe708a90f4a9d8e0569fc62c13
Your Name <1331derek@gmail.com>
2018-06-24 10:37:53 -0600

    hmmmm

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,155 +6,6 @@ var intervalid;
 
 
 
-
-
-
-
-var triviaQuestions = [,
-  
-  
-  "Who goes by the nickname The Poker Brat?",
-  "Who is credited with starting the Poker Boom?",
-  "What year did Black Friday happen?",
-  "Who is the best poker player ever?",
-  "What Online Poker Website defrauded players out of more than $300 million?",
-  'Who was not involved in "the Most Exclusive, High-Stakes Underground Poker Game in the World?"']
-
-var triviaAnswers = {
-
-  answers3: ["mytommyhurts", "durrrr", "dwanoftheman", "dwanofthewoman"],
-  answers4: ["Phil Ivey", "Phil Hellmuth", "Phil Bratswoski", "Philip J Fry"],
-  answers5: ["George W Bush", "Chris Moneymaker", "Adam Dolladollabillsya", "Anthony Wiener"],
-  answers6: ["2009", "ummm the Friday after Thanksgiving", "2010", "2011"],
-  answers7: ["Derek Lee Reed", "Phil Ivey", "Matt Damon", "Kendell Jenner"],
-  answers8: ["Full Tilt Poker", "Party Poker", "Pokerstars", "Trilogy Poker"],
-  answers9: ["Ben Affleck", "Macaulay Culkin", "Michael Cera", "Leonardo DiCaprio"]
-};
-
-function remover() {
-  $("#answer1").empty();
-  $("#answer2").empty();
-  $("#answer3").empty();
-  $("#answer4").empty();
-  wrong++;
-
-}
-
-function remover2() {
-  $("#answer1").empty();
-  $("#answer2").empty();
-  $("#answer3").empty();
-  $("#answer4").empty();
-  right++;
-}
-
-function Question1() {
-
-  run();
-  $('#questions').html('Who won the first World Series of Poker: Main Event?');
-
-  $('#answer1').html('Johnny Leaf').click(function () {
-    loser();
-    setTimeout(Question2, 5000);
-  });
-  $('#answer2').html('Johhny Moss').click(function () {
-    winner();
-    setTimeout(Question2, 5000);
-  });
-  $('#answer3').html('Johnny Appleseed').click(function () {
-    loser();
-    setTimeout(Question2, 5000);
-  });
-  $('#answer4').html('Johnny Bananas').click(function () {
-    loser();
-    setTimeout(Question2, 5000);
-  });
-}
-
-function Question2() {
-
-
-  run()
-
-  $('#questions').html('What was the largest WSOP: Main Event 1st Place?');
-
-  $('#answer1').html('9 Ladies Dancing').click(function () {
-    loser();
-    setTimeout(Question3, 5000);
-  });
-
-  $('#answer2').html('$9,000,000').click(function () {
-    loser();
-    setTimeout(Question3, 5000);
-  });
-
-  $('#answer3').html('about tree fiddy').click(function () {
-    loser();
-    setTimeout(Question3, 5000);
-  });
-
-  $('#answer4').html('$12,000,000').click(function () {
-    winner();
-    setTimeout(Question3, 5000);
-  });
-
-}
-function Question3() {
-
-
-  run()
-
-  $('#questions').html('What was Tom Dwans online screen name?');
-
-  $('#answer1').html('mytommyhurts').click(function () {
-    loser();
-    setTimeout(Question4, 5000);
-  });
-
-  $('#answer2').html('durrr').click(function () {
-    winner();
-    setTimeout(Question4, 5000);
-  });
-
-  $('#answer3').html('dwanoftheman').click(function () {
-    loser();
-    setTimeout(Question4, 5000);
-  });
-
-  $('#answer4').html('dwanofthewoman').click(function () {
-    loser();
-    setTimeout(Question4, 5000);
-  });
-}
-function Question4() {
-
-
-  run()
-
-  $('#questions').html('What was Tom Dwans online screen name?');
-
-  $('#answer1').html('mytommyhurts').click(function () {
-    loser();
-    setTimeout(Question2, 5000);
-  });
-
-  $('#answer2').html('durrr').click(function () {
-    winner();
-    setTimeout(Question2, 5000);
-  });
-
-  $('#answer3').html('dwanoftheman').click(function () {
-    loser();
-    setTimeout(Question2, 5000);
-  });
-
-  $('#answer4').html('dwanofthewoman').click(function () {
-    loser();
-    setTimeout(Question2, 5000);
-  });
-}
-
-
 function start() {
   $('#start').hide();
   $('#timer').show();
@@ -167,7 +18,17 @@ function start() {
 function restart() {
   wrong = 0;
   right = 0;
+  stop()
+  $("#timer").empty();
+  $("#answer1").empty();
+  $("#answer2").empty();
+  $("#answer3").empty();
+  $("#answer4").empty();
+  $("#questions").empty();
+  $('#start').show();
+
 }
+
 
 function run() {
   clearInterval(intervalid);
@@ -175,38 +36,38 @@ function run() {
 }
 
 function loser() {
-  stop();
-  remover();
-
   $('#answer1').html('Correct: ' + right);
   $('#answer2').html('Incorrect: ' + wrong);
-  $('#answer3').html('<img src="assets/images/win.gif">');
+  $('#answer3').html('<img src="assets/images/loser.gif">');
+  $('#answer4').html('');
 
+ 
 }
-
 
 function winner() {
-  stop();
-  remover2();
-
   $('#answer1').html('Correct: ' + right);
   $('#answer2').html('Incorrect: ' + wrong);
   $('#answer3').html('<img src="assets/images/win.gif">');
-
-
+  $('#answer4').html('');
 }
-
+  
 function decrement() {
-  $('#timer').html('Time Remaining: ' + time);
+
+
   time--;
-  if (time === -1) {
-    stop()
+
+
+  $("#timer").html("<h2>" + time + "</h2>");
+
+
+
+  if (time === 0) {
+
+
+    stop();
+
   }
 }
-
-
-
-
 
 function stop() {
   clearInterval(intervalid);
@@ -214,7 +75,169 @@ function stop() {
 
 $(window).on('load', function () {
   $('#timer').hide();
-})
+  $('#reset').hide();
+});
+
+
+function Question1() {
+
+  run();
+
+  $('#questions').html('1. Who won the first World Series of Poker: Main Event?');
+
+  $('#answer1').html('Johnny Leaf').click(function () {
+    // stop()
+    loser();
+    setTimeout(Question2, 3000);
+  });
+  $('#answer2').html('Johhny Moss').click(function () {
+    right++;
+    winner();
+    setTimeout(Question2, 3000);
+  });
+  $('#answer3').html('Johnny Appleseed').click(function () {
+
+    loser();
+    setTimeout(Question2, 3000);
+  });
+  $('#answer4').html('Johnny Bananas').click(function () {
+
+    loser();
+    setTimeout(Question2, 3000);
+  });
+}
+
+function Question2() {
+
+
+  time = 30;
+  run()
+
+  $('#questions').html('2. What was the largest WSOP: Main Event 1st Place?');
+
+  $('#answer1').html('9 Ladies Dancing').click(function () {
+
+
+    loser();
+    setTimeout(Question3, 3000);
+  });
+
+  $('#answer2').html('$9,000,000').click(function () {
+
+
+    loser();
+    setTimeout(Question3, 3000);
+  });
+
+  $('#answer3').html('about tree fiddy').click(function () {
+
+
+    loser();
+    setTimeout(Question3, 3000);
+  });
+
+  $('#answer4').html('$12,000,000').click(function () {
+    right++;
+    winner();
+    setTimeout(Question3, 3000);
+  });
+
+}
+
+function Question3() {
+
+
+  
+  run()
+
+
+  $('#questions').html('3. What was Tom Dwans online screen name?');
+
+  $('#answer1').html('mytommyhurts').click(function () {
+    loser();
+    setTimeout(Question4, 3000);
+  });
+
+  $('#answer2').html('durrr').click(function () {
+    right++;
+    winner();
+    setTimeout(Question4, 3000);
+  });
+
+  $('#answer3').html('dwanoftheman').click(function () {
+    loser();
+    setTimeout(Question4, 3000);
+  });
+
+  $('#answer4').html('dwanofthewoman').click(function () {
+    loser();
+    setTimeout(Question4, 3000);
+  });
+}
+
+function Question4() {
+
+
+  
+  run()
+
+
+  $('#questions').html('4. What Online Poker Website defrauded players out of more than $300 million?');
+
+  $('#answer1').html('Full Tilt Poker').click(function () {
+    right++;
+    winner();
+    setTimeout(Question5, 3000);
+  });
+
+  $('#answer2').html('Party Poker').click(function () {
+    loser();
+    setTimeout(Question5, 3000);
+  });
+
+  $('#answer3').html('Pokerstars').click(function () {
+    loser();
+    setTimeout(Question5, 3000);
+  });
+
+  $('#answer4').html('Trilogy Poker').click(function () {
+    loser();
+    setTimeout(Question5, 3000);
+  });
+}
+
+function Question5() {
+
+
+  
+  run()
+
+
+  $('#questions').html('5. Who is credited with starting the Poker Boom?');
+
+  $('#answer1').html('George W Bush').click(function () {
+    loser();
+    setTimeout(restart, 3000);
+  });
+
+  $('#answer2').html('Adam Dolladollabillsya').click(function () {
+    loser();
+    setTimeout(restart, 3000);
+  });
+
+  $('#answer3').html('Anthony Wiener').click(function () {
+    loser();
+    setTimeout(restart, 3000);
+  });
+
+  $('#answer4').html('Chris Moneymaker').click(function () {
+    right++;
+    winner();
+    setTimeout(restart, 3000);
+  });
+}
+
+
 
 
 
