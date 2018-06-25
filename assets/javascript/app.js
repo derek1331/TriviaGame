824244976dce5fcdf165549288703dddbd3a5a11
Your Name <1331derek@gmail.com>
2018-06-25 03:02:39 -0600

    scrappy doo

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,89 @@
-
-var time = 30;
-var wrong = 0;
+// Variables
+var time = 16;
 var right = 0;
+var wrong = 0;
 var intervalid;
+var correct = ["Johhny Moss", "$12,000,000", "durrr", "Full Tilt Poker", "Chris Moneymaker"]
 
 
-
-function start() {
-  $('#start').hide();
-  $('#timer').show();
-
-  Question1();
-
-
+function rightanswer() {
+  right++;
 }
 
+function wronganswer() {
+  wrong++;
+}
+
+//Wrong answer function
+function loser() {
+ 
+  stop();
+  time = 16;
+  $("#timer").hide();
+  $("#answers").hide();
+  $("#score").show();
+  $("#score").html("Right: " + right + "    Wrong: " + wrong + "<br>" + "<br>" + '<img src="assets/images/loser.gif">' + "<br>" + "<br>");
+  $("#correct").show()
+}
+
+
+//Right answer function
+function winner() {
+  
+  stop();
+  time = 16;
+  $("#timer").hide();
+  $("#answers").hide();
+  $("#correct").hide();
+  $("#score").show();
+  $("#score").html("Right: " + right + "    Wrong: " + wrong + '<br>' + '<br>' + '<img src="assets/images/win.gif">');
+}
+
+
+
+// Start trivia button
+function start() {
+  $("#correct").hide()
+  $("#score").hide()
+  $("#start").hide();
+  $("#answers").show()
+  $("#timer").show();
+  $("#questions").show();
+  run();
+  setTimeout(question1, 1000);
+}
+
+//Restart
 function restart() {
   wrong = 0;
   right = 0;
-  stop()
+  time = 16;
+  stop();
+  $("#correct").empty();
+  $("#answers").hide();
   $("#timer").empty();
-  $("#answer1").empty();
-  $("#answer2").empty();
-  $("#answer3").empty();
-  $("#answer4").empty();
   $("#questions").empty();
-  $('#start').show();
+  $("#score").show();
+  $("#score").html("How did you do?" + '<br>' + '<br>' + "Right: " + right + "    Wrong: " + wrong);
+  $("#start").show();
 
 }
 
 
+
+// Timer
 function run() {
   clearInterval(intervalid);
   intervalid = setInterval(decrement, 1000);
 }
 
-function loser() {
-  $('#answer1').html('Correct: ' + right);
-  $('#answer2').html('Incorrect: ' + wrong);
-  $('#answer3').html('<img src="assets/images/loser.gif">');
-  $('#answer4').html('');
-
- 
-}
-
-function winner() {
-  $('#answer1').html('Correct: ' + right);
-  $('#answer2').html('Incorrect: ' + wrong);
-  $('#answer3').html('<img src="assets/images/win.gif">');
-  $('#answer4').html('');
-}
-  
 function decrement() {
-
 
   time--;
 
-
-  $("#timer").html("<h2>" + time + "</h2>");
-
-
+  $("#timer").html(time + " Seconds Left");
 
   if (time === 0) {
-
-
     stop();
 
   }
@@ -73,172 +93,231 @@ function stop() {
   clearInterval(intervalid);
 }
 
-$(window).on('load', function () {
-  $('#timer').hide();
-  $('#reset').hide();
-});
 
+// Questions and Answers. Probably should have set them as varaibles but oh well
+// Couldn't figure out how to get it to switch to losers fujction when time === 0. Found a workaround but it screwed up the clicks.
 
-function Question1() {
+function question1() {
+
+  $("#questions").html("1. Who won the first World Series of Poker: Main Event?");
+
+  $("#answer1").html("Johnny Leaf").click(function () {
+    $("#correct").html("Correct Answer: " + correct[0].toString());
+
+    loser();
+    setTimeout(question2, 4500);
+  });
+
+  $("#answer2").html("Johhny Moss").click(function () {
+    rightanswer();
+    winner();
+    setTimeout(question2, 4500);
+  });
+
+  $("#answer3").html("Johnny Appleseed").click(function () {
+    $("#correct").html("Correct Answer: " + correct[0].toString());
+
+    loser();
+    setTimeout(question2, 4500);
+  });
+
+  $("#answer4").html("Johnny Bananas").click(function () {
+    $("#correct").html("Correct Answer: " + correct[0].toString());
+
+    loser();
+    setTimeout(question2, 4500);
+  });
+}
+
+function question2() {
 
   run();
 
-  $('#questions').html('1. Who won the first World Series of Poker: Main Event?');
+  $("#start").hide();
 
-  $('#answer1').html('Johnny Leaf').click(function () {
-    // stop()
+  $("#score").hide();
+
+  $("#correct").hide();
+
+
+  $("#timer").show();
+
+  $("#answers").show();
+
+  $("#questions").html("2. What was the largest WSOP: Main Event 1st Place prize?");
+
+  $("#answer1").html("9 Ladies Dancing").click(function () {
+    $("#correct").html("Correct Answer: " + correct[1].toString());
     loser();
-    setTimeout(Question2, 3000);
+    setTimeout(question3, 4500);
   });
-  $('#answer2').html('Johhny Moss').click(function () {
-    right++;
+
+  $("#answer2").html("$9,000,000").click(function () {
+    $("#correct").html("Correct Answer: " + correct[1].toString());
+
+    loser();
+    setTimeout(question3, 4500);
+  });
+
+  $("#answer3").html("about tree fiddy").click(function () {
+    $("#correct").html("Correct Answer: " + correct[1].toString());
+    loser();
+    setTimeout(question3, 4500);
+  });
+
+  $("#answer4").html("$12,000,000").click(function () {
+    rightanswer();
     winner();
-    setTimeout(Question2, 3000);
-  });
-  $('#answer3').html('Johnny Appleseed').click(function () {
-
-    loser();
-    setTimeout(Question2, 3000);
-  });
-  $('#answer4').html('Johnny Bananas').click(function () {
-
-    loser();
-    setTimeout(Question2, 3000);
-  });
-}
-
-function Question2() {
-
-
-  time = 30;
-  run()
-
-  $('#questions').html('2. What was the largest WSOP: Main Event 1st Place?');
-
-  $('#answer1').html('9 Ladies Dancing').click(function () {
-
-
-    loser();
-    setTimeout(Question3, 3000);
-  });
-
-  $('#answer2').html('$9,000,000').click(function () {
-
-
-    loser();
-    setTimeout(Question3, 3000);
-  });
-
-  $('#answer3').html('about tree fiddy').click(function () {
-
-
-    loser();
-    setTimeout(Question3, 3000);
-  });
-
-  $('#answer4').html('$12,000,000').click(function () {
-    right++;
-    winner();
-    setTimeout(Question3, 3000);
+    setTimeout(question3, 4500);
   });
 
 }
 
-function Question3() {
+function question3() {
 
+  run();
 
-  
-  run()
+  $("#start").hide();
 
+  $("#score").hide();
 
-  $('#questions').html('3. What was Tom Dwans online screen name?');
+  $("#correct").hide();
 
-  $('#answer1').html('mytommyhurts').click(function () {
+  $("#timer").show();
+
+  $("#answers").show();
+
+  $("#questions").html("3. What was Tom Dwans online screen name?");
+
+  $("#answer1").html("mytommyhurts").click(function () {
+    $("#correct").html("Correct Answer: " + correct[2].toString());
     loser();
-    setTimeout(Question4, 3000);
+    setTimeout(question4, 4500);
   });
 
-  $('#answer2').html('durrr').click(function () {
-    right++;
+  $("#answer2").html("durrr").click(function () {
+    rightanswer();
     winner();
-    setTimeout(Question4, 3000);
+    setTimeout(question4, 4500);
   });
 
-  $('#answer3').html('dwanoftheman').click(function () {
+  $("#answer3").html("dwanoftheman").click(function () {
+    $("#correct").html("Correct Answer: " + correct[2].toString());
     loser();
-    setTimeout(Question4, 3000);
+    setTimeout(question4, 4500);
   });
 
-  $('#answer4').html('dwanofthewoman').click(function () {
+  $("#answer4").html("dwanofthewoman").click(function () {
+    $("#correct").html("Correct Answer: " + correct[2].toString());
     loser();
-    setTimeout(Question4, 3000);
-  });
-}
-
-function Question4() {
-
-
-  
-  run()
-
-
-  $('#questions').html('4. What Online Poker Website defrauded players out of more than $300 million?');
-
-  $('#answer1').html('Full Tilt Poker').click(function () {
-    right++;
-    winner();
-    setTimeout(Question5, 3000);
-  });
-
-  $('#answer2').html('Party Poker').click(function () {
-    loser();
-    setTimeout(Question5, 3000);
-  });
-
-  $('#answer3').html('Pokerstars').click(function () {
-    loser();
-    setTimeout(Question5, 3000);
-  });
-
-  $('#answer4').html('Trilogy Poker').click(function () {
-    loser();
-    setTimeout(Question5, 3000);
-  });
-}
-
-function Question5() {
-
-
-  
-  run()
-
-
-  $('#questions').html('5. Who is credited with starting the Poker Boom?');
-
-  $('#answer1').html('George W Bush').click(function () {
-    loser();
-    setTimeout(restart, 3000);
-  });
-
-  $('#answer2').html('Adam Dolladollabillsya').click(function () {
-    loser();
-    setTimeout(restart, 3000);
-  });
-
-  $('#answer3').html('Anthony Wiener').click(function () {
-    loser();
-    setTimeout(restart, 3000);
-  });
-
-  $('#answer4').html('Chris Moneymaker').click(function () {
-    right++;
-    winner();
-    setTimeout(restart, 3000);
+    setTimeout(question4, 4500);
   });
 }
 
 
+function question4() {
+
+  run();
+
+  $("#start").hide();
+
+  $("#score").hide();
+
+  $("#correct").hide();
+
+  $("#timer").show();
+
+  $("#answers").show();
+
+  $("#questions").html("4. What Online Poker Website defrauded players out of more than $300 million?");
+
+  $("#answer1").html("Full Tilt Poker").click(function () {
+    winner();
+    setTimeout(question5, 4500);
+  });
+
+  $("#answer2").html("Party Poker").click(function () {
+    $("#correct").html("Correct Answer: " + correct[3].toString());
+    loser();
+    setTimeout(question5, 4500);
+  });
+
+  $("#answer3").html("Pokerstars").click(function () {
+    $("#correct").html("Correct Answer: " + correct[3].toString());
+    loser();
+    setTimeout(question5, 4500);
+  });
+
+  $("#answer4").html("Trilogy Poker").click(function () {
+    $("#correct").html("Correct Answer: " + correct[3].toString());
+    loser();
+    setTimeout(question5, 4500);
+  });
+}
+
+function question5() {
+
+  run();
+
+  $("#start").hide();
+
+  $("#score").hide();
+
+  $("#correct").hide();
+
+  $("#timer").show();
+
+  $("#answers").show();
+
+  $("#questions").html("5. Who is credited with starting the Poker Boom?");
+
+  $("#answer1").html("George W Bush").click(function () {
+    $("#correct").html("Correct Answer: " + correct[4].toString());
+    loser();
+    setTimeout(restart, 4500);
+  });
+
+  $("#answer2").html("Adam Dolladollabillsya").click(function () {
+    $("#correct").html("Correct Answer: " + correct[4].toString());
+    loser();
+    setTimeout(restart, 4500);
+  });
+
+  $("#answer3").html("Anthony Wiener").click(function () {
+    $("#correct").html("Correct Answer: " + correct[4].toString());
+    loser();
+    setTimeout(restart, 4500);
+  });
+
+  $("#answer4").html("Chris Moneymaker").click(function () {
+    winner();
+    setTimeout(restart, 4500);
+  });
+}
+
+// Didn't Work
+
+  // var time = 16;
+
+  // if (time === 0) {
+
+  //   loser();
+  //   setTimeout(question2, 4500);
+  // };
 
 
 
+// Screwy time work around
 
+  // setTimeout(function() {
+    //   loser();
+
+
+   //   setTimeout(function() {
+   //     stop();
+   //     question2();
+   //   }, 4500);
+    // },15000);
+
+    // while( (i = correct.shift()) !== undefined ) {
+//   console.log(i);}
